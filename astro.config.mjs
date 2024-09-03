f5df28db5bde4af63b1bb832770c6fce1dd1068f
Aleksi <liukkonenaleksi@gmail.com>
2024-09-03 08:12:00 +0300

    Add basic authentication

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), clerk()],
+  output: 'server',
+  adapter: node({ mode: "standalone" }),
 });
