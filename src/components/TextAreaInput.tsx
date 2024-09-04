576a8015a5eaa5be15e163f4d5e2eaf1840203f3
Aleksi <liukkonenaleksi@gmail.com>
2024-09-04 11:18:49 +0300

    Add opacity to textareainput

--- src/components/TextAreaInput.tsx
+++ src/components/TextAreaInput.tsx
@@ -12,7 +12,7 @@ const TextAreaInput: React.FC<TextAreaInputProps> = ({ label, value, onChange, p
     <div className="border border-neutral-500 rounded-lg p-4 mt-6">
       <h2 className="text-white text-lg mb-4 text-center">{label}</h2>
       <textarea
-        className="w-full bg-black border border-neutral-500 text-white p-4 rounded-lg resize-none"
+        className="w-full bg-black border bg-opacity-60 border-neutral-500 text-white p-4 rounded-lg resize-none"
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder}
