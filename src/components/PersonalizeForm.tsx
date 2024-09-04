5198dc2d7e2865b0e6341655b9ac4eb416461900
Aleksi <liukkonenaleksi@gmail.com>
2024-09-04 10:36:00 +0300

    Adjust form

--- src/components/PersonalizeForm.tsx
+++ src/components/PersonalizeForm.tsx
@@ -89,7 +89,7 @@ const PersonalizeForm: React.FC<PersonalizeFormProps> = ({ formData, handleChang
   };
 
   const fieldIcons = {
-/*  age: (
+/*   age: (
       <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z" />
       </svg>
@@ -172,14 +172,16 @@ const PersonalizeForm: React.FC<PersonalizeFormProps> = ({ formData, handleChang
         )}
       </div>
       {!selectedField && (
+        <div className="flex justify-center mb-2 md:m-0">
         <button
           type="submit"
           className="bg-white text-black px-6 py-2 rounded-full hover:bg-gray-200 transition-colors"
           disabled={loading}
           onClick={handleSubmit}
         >
-          {loading ? <LoadingSpinner text="Generating Plan..."/> : 'Get Your Fitness Plan'}
+          {loading ? <LoadingSpinner text="Creating your plan..."/> : 'Create your fitness plan'}
         </button>
+      </div>
       )}
     </>
   );
